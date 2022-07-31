7ac7e414e6ba1de9cfd464b1e00411c5e29cce9d
curiousengg <107173181+curiousengg@users.noreply.github.com>
2022-07-31 12:36:32 -0700

    Update csv_join.py

--- tools/bag_processing/scripts/csv_join.py
+++ tools/bag_processing/scripts/csv_join.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-v",
         "--verbose",
-        help="print more debug info",
+        help="print more debug information",
         action="store_true",
     )
 
